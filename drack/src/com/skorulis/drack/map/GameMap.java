06d7acfdd7c6c5207af4eb6d74ddb257d2f6f6f7
Alexander Skorulis <skorulis@gmail.com>
2014-04-10 16:15:17 +1000

    Drawing the map floor.

--- drack/src/com/skorulis/drack/map/GameMap.java
+++ drack/src/com/skorulis/drack/map/GameMap.java
@@ -1,16 +1,11 @@
 package com.skorulis.drack.map;
 
 import java.util.ArrayList;
-
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
-import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Disposable;
@@ -19,10 +14,7 @@ import com.skorulis.scene.SceneNode;
 public class GameMap implements SceneNode, Disposable{
 
 	private Matrix4 transform;
-	private Model blockModel;
-	
-	private ArrayList<ModelInstance> walls;
-	
+
 	private int width,depth;
 	private MapSquare[][] squares;
 	
@@ -32,35 +24,11 @@ public class GameMap implements SceneNode, Disposable{
 		squares = new MapSquare[depth][width];
 		for(int i = 0; i < depth; ++i) {
 			for(int j = 0; j < width; ++j) {
-				squares[i][j] = new MapSquare(assets);
+				squares[i][j] = new MapSquare(assets,j,i);
 			}
 		}
 		
 		transform = new Matrix4();
-		
-		blockModel = assets.get("block", Model.class);
-		
-		walls = new ArrayList<ModelInstance>();
-		for(int i = -1; i < width + 1; ++i) {
-			ModelInstance wallInstance = new ModelInstance(blockModel);
-			wallInstance.transform.setToTranslation(new Vector3(i + 0.5f, 0.5f, -0.5f));
-			walls.add(wallInstance);
-			
-			wallInstance = new ModelInstance(blockModel);
-			wallInstance.transform.setToTranslation(new Vector3(i + 0.5f, 0.5f, depth + 0.5f));
-			walls.add(wallInstance);
-		}
-		
-		for(int i = 0; i < depth; ++i) {
-			ModelInstance wallInstance = new ModelInstance(blockModel);
-			wallInstance.transform.setToTranslation(new Vector3(-0.5f, 0.5f, i + 0.5f));
-			walls.add(wallInstance);
-			
-			wallInstance = new ModelInstance(blockModel);
-			wallInstance.transform.setToTranslation(new Vector3(width + 0.5f, 0.5f, i + 0.5f));
-			walls.add(wallInstance);
-		}
-		
 	}
 	
 	@Override
@@ -75,8 +43,6 @@ public class GameMap implements SceneNode, Disposable{
 
 	@Override
 	public void render(ModelBatch batch, Environment environment) {
-		batch.render(walls,environment);
-		
 		for(int i = 0; i < depth; ++i) {
 			for(int j = 0; j < width; ++j) {
 				squares[i][j].render(batch, environment);
--- drack/src/com/skorulis/drack/map/MapSquare.java
+++ drack/src/com/skorulis/drack/map/MapSquare.java
@@ -2,29 +2,47 @@ package com.skorulis.drack.map;
 
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector3;
 import com.skorulis.scene.SceneNode;
 
 public class MapSquare implements SceneNode {
 
-	public MapSquare(AssetManager assets) {
-		
+	private ModelInstance instance;
+	private final int x;
+	private final int z;
+	
+	public MapSquare(AssetManager assets,int x, int z) {
+		this.x = x;
+		this.z = z;
+		instance = new ModelInstance(assets.get("block", Model.class));
+		instance.transform.setToTranslation(new Vector3(x, 0.5f, z));
 	}
 	
 	@Override
 	public Matrix4 absTransform() {
-		return null;
+		return instance.transform;
 	}
 
 	@Override
 	public Matrix4 relTransform() {
-		return null;
+		return instance.transform;
 	}
 
 	@Override
 	public void render(ModelBatch batch, Environment environment) {
-		
+		batch.render(instance,environment);
 	}
 
+	public int x() {
+		return x;
+	}
+	
+	public int z() {
+		return z;
+	}
+	
 }
