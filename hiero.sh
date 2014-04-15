807447b0c3016d38b0539088bf12c1f6317996fe
Alexander Skorulis <skorulis@gmail.com>
2014-04-15 14:46:18 +1000

    Created new font. Not really that useful

---
+++ hiero.sh
@@ -0,0 +1,2 @@
+export CLASSPATH=~/dev/gdx/libgdx-nightly-20140410/extensions/gdx-tools/gdx-tools.jar:~/dev/gdx/libgdx-nightly-20140410/gdx.jar:~/dev/gdx/libgdx-nightly-20140410/gdx-natives.jar:~/dev/gdx/libgdx-nightly-20140410/gdx-backend-lwjgl.jar:~/dev/gdx/libgdx-nightly-20140410/gdx-backend-lwjgl-natives.jar
+java com.badlogic.gdx.tools.hiero.Hiero
